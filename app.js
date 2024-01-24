addbdd8ed2e6b70f4506c79f366fb82d7f5cb36b
Anik Acharjee <anikacharjee@users.noreply.github.com>
2024-01-24 08:23:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,3 +32,54 @@ const authenticate = (req, res, next) => {
   }
 };
 
+//Empty array
+let todos = [];
+
+//Routes
+
+//Home Route
+app.get('/', authenticate, (req, res) => {
+  res.render('index', { user: req.session.user, todos});
+});
+
+//Login Route
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req,res) => {
+  const {username, password} = req.body;
+
+  if (username === 'user' && password === 'password') {
+    req.session.user = username;
+    res.redirect('/');
+  } else {
+    res.render('login', { error : 'Invalid username or password'});
+  }
+});
+
+//logout route
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/login');
+});
+
+//Todo Route
+app.post('/add', authenticate, (req, res) => {
+  const { todo } = req.body;
+  todos.push({ text: todo, done: false });
+  res.redirect('/');
+});
+
+//Mark Todo as Done route
+app.get('/done/:index', authenticate, (req, res) => {
+  const index = parseInt(req.params.index, 10);
+  if(!isNaN(index) && index >= 0 && index < todo.length) {
+    todos[index].done = true;
+  }
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
